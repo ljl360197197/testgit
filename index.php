38c20567d31fd2973f8fd2fc8d95ec3e41c93ea2
liaojinling <360197197@qq.com>
2014-03-02 08:46:05 +0800

    mytest

--- index.php
+++ index.php
@@ -5,4 +5,14 @@ echo "liiiiiao";
 echo "this in dev2";
 echo "linlinlin";
 echo "test";
+$arr = array();
+$arr["nihao"] = "nihao";
+$arr["te"] = 2;
+$arr["sl"] = "sdf";
+print_var($arr);
+function print_var($var) {
+	echo "<pre>";
+	echo $var;
+	echo "</pre>";
+}
 ?>
